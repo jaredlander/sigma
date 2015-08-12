132f02dc63e56facdf360a871eb589a10db5a173
Jared Lander <info@jaredlander.com>
2015-08-11 23:44:26 -0400

    Testing to make sure assignPositions returns the proper number of items.

--- tests/testthat/test-assignPositions.R
+++ tests/testthat/test-assignPositions.R
@@ -13,3 +13,13 @@ test_that('assignPositions left the right amount of vertices', {
     expect_equal(vcount(g1), vcount(flightGraph))
     expect_equal(vcount(g2), vcount(flightGraph))
 })
+
+test_that('assignPositions left behind x,y,z', {
+    expect_equal(length(V(g1)$x), vcount(g1))
+    expect_equal(length(V(g1)$y), vcount(g1))
+    expect_equal(length(V(g1)$z), vcount(g1))
+    
+    expect_equal(length(V(g2)$x), vcount(g2))
+    expect_equal(length(V(g2)$y), vcount(g2))
+    expect_equal(length(V(g2)$z), vcount(g2))
+})
