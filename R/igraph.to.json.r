7652164837f46af796ec58d9208c210052d58bb1
Jared Lander <info@jaredlander.com>
2015-08-11 23:43:54 -0400

    Built helper function for extracting graph info.  Renamed igraph.to.json to igraphToJson.

--- R/igraph.to.json.r
+++ R/igraph.to.json.r
@@ -1,18 +1,25 @@
-#' @title igraph.to.json
+#' @title igraphToJson
 #' @description Convert an igraph object to json
 #' @details Converts an igraph object to json with an array holding an array each for nodes and edges.
 #' @author Jared P. Lander
-#' @aliases igraph.to.json
-#' @export igraph.to.json
-#' @importFrom igraph get.data.frame
+#' @aliases igraphToJson
+#' @export igraphToJson
 #' @importFrom jsonlite toJSON
 #' @param g An igraph object
 #' @return A string of json text representing the igraph object as an array holding an array each for nodes and edges.
 #' @examples 
 #' data(flightGraph)
-#' igraph.to.json(flightGraph)
+#' igraphToJson(flightGraph)
 #' 
-igraph.to.json <- function(g)
+igraphToJson <- function(g)
+{
+    # convert graph into a list of data.frames
+    gdata<- prepIgraphConversion(g)
+    
+    return(toJSON(gdata))
+}
+
+prepIgraphConversion <- function(g)
 {
     # make graph into a list of data.frames
     gdata <- get.data.frame(g, what = "both")
@@ -20,5 +27,40 @@ igraph.to.json <- function(g)
     # rename vertices to nodes
     names(gdata)[1] <- 'nodes'
     
-    return(toJSON(gdata))
+    # add a column to nodes to hold the IDs
+    gdata$nodes %<>% mutate(id=as.integer(as.factor(name))) %>% rename(label=name)
+    # extract node info for color, position, size, shape and color
+    
+    ## replace from and to in edges with their IDs
+    # first create a named vector of ids
+    nodesID <- gdata$nodes$id
+    names(nodesID) <- gdata$nodes$label
+    # now in the edges create the id columns and an edge id
+    gdata$edges %<>% mutate(id=sprintf('e%s', 1:n()), source=nodesID[from], target=nodesID[to]) %>%
+        # and drop the old one
+        select(-from, -to)
+    
+    return(gdata)
 }
+hike <- prepIgraphForGexf(flightGraph)
+
+
+#' @title igraphToGexf
+#' @description Convert an igraph object to json
+#' @details Converts an igraph object to json with an array holding an array each for nodes and edges.
+#' @author Jared P. Lander
+#' @aliases igraphToGexf
+#' @export igraphToGexf
+#' @importFrom jsonlite toJSON
+#' @param g An igraph object
+#' @return A string of json text representing the igraph object as an array holding an array each for nodes and edges.
+#' @examples 
+#' data(flightGraph)
+#' igraphToGexf(flightGraph)
+#' 
+igraphToGexf <- function(g)
+{
+    # convert graph into a list of data.frames
+    gdata<- prepIgraphConversion(g)
+}
+
